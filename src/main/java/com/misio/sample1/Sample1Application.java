cd9e2035b0740e1402ce4c67815e5a094732066b
SIC\msherafatian <msherafatian@sadad.co.ir>
2023-08-26 17:54:25 +0330

    Add runner

--- src/main/java/com/misio/sample1/Sample1Application.java
+++ src/main/java/com/misio/sample1/Sample1Application.java
@@ -1,13 +1,18 @@
 package com.misio.sample1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sample1Application {
+public class Sample1Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Sample1Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
