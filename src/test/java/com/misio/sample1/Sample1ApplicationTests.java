4bc970a02aeaa50286fbfb8157e2b8e8d092c04e
SIC\msherafatian <msherafatian@sadad.co.ir>
2023-08-26 10:35:23 +0330

    Initial Spring project

---
+++ src/test/java/com/misio/sample1/Sample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misio.sample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
